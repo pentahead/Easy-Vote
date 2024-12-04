aa34530b674c56ef8eef3ed4af5c4054c0eac3f8
AZR88 <azrielamaldany@gmail.com>
2024-12-04 22:05:00 +0700

    update : profile.jsx

--- src/routes/profile.jsx
+++ src/routes/profile.jsx
@@ -15,6 +15,7 @@ import {
 import NavigationBar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Pic from "../assets/user.png";
+import "../styles/profile.css";
 export const Route = createFileRoute("/profile")({
   component: Index,
 });
@@ -23,20 +24,23 @@ const Profile = () => {
   return <div className="Profile ">
 
 
-  <div className="container" style={{ width: "100vh",height: "100vh",marginTop:"10vh", justifyContent: "center", display: "flex" }}>
-    <Card style={{ width: "70%", height: "70%", borderRadius: "20px", display: "flex", justifyContent: "center", alignItems: "center"  }}>
-      <p style={{fontSize:"5vh",fontWeight:"bold", alignContent:"center", justifyContent:"center", margin:"0"}}>
-        PROFILE</p>
-      <img src={Pic} alt="user" style={{ width: "40%", height: "40%", borderRadius: "20px", padding:"5%" }} />
+  <div className="container-prof" >
+    <Card className="Card" >
+      <div className="Rectangle">
+      <p className="Title">
+      PROFILE
+      </p>
+      </div>
+      <img src={Pic} alt="user" className="Pic" />
       <h2>NAMA</h2>
-        <div style={{width: "90%", height: "10%", borderRadius: "20px", display: "flex", justifyContent: "center", alignItems: "center", alignContent: "center",border: "1px solid black",  padding:"5%", margin:"2.5%"}}>
-          <p style={{fontSize:"3vh",fontWeight:"bold", alignContent:"center", justifyContent:"center", margin:"0"}}>
-            NUR BASHORI RAHMAT NUR 
+        <div className="Information" >
+          <p >
+            BUZZ LIGHYEAR
           </p>  
         </div>
       <h2>NIM</h2>
-        <div style={{width: "90%", height: "10%", borderRadius: "20px", display: "flex", justifyContent: "center", alignItems: "center", alignContent: "center",border: "1px solid black", padding:"5%", margin:"2.5%"}}>
-          <p style={{fontSize:"3vh",fontWeight:"bold", alignContent:"center", justifyContent:"center",margin:"0"}}>222410102037</p>
+        <div className="Information" >
+          <p >222410102037</p>
         </div>
     </Card>
   </div>
