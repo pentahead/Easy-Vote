ce9e3e0c940003fb3fdd44f84af50aa2cfe2775c
AZR88 <azrielamaldany@gmail.com>
2024-12-06 00:38:30 +0700

    update : Package.json + add candidate

--- src/routes/candidate.jsx
+++ src/routes/candidate.jsx
@@ -14,13 +14,88 @@ import {
 } from 'react-bootstrap'
 import NavigationBar from '../components/Navbar'
 import Footer from '../components/Footer'
+import pic from '../assets/person.png'
+import '../styles/candidate.css'
+import { a } from '@react-spring/web'
 
 export const Route = createFileRoute('/candidate')({
   component: Index,
 })
 
 const Candidates = () => {
-  return <div className="Candidates"></div>
+  return (
+    <div className="Candidates">
+      <div className="container-candidates">
+     
+      <Card className='kiwkiw'>
+        <Card.Img variant="top" src={pic} alt="Card image" />
+        <Card.Body>
+        <Card.Title className='card-title'>RADEN MAS FARUQ </Card.Title>
+        <div className='visi'>
+          <table className="table-visi">
+              <tr>
+                  <th>
+                      <div className="header-th">Visi</div>
+                      <div className="content">1. Membangun negeri ramah</div>
+                  </th>
+              </tr>
+              <tr>
+                  <th>
+                      <div className="header-th">Misi</div>
+                      <div className="content">1. Membangun negeri ramah</div>
+                  </th>
+              </tr>
+           </table>
+          </div>
+        </Card.Body>
+      </Card>
+      <Card className='kiwkiw'>
+        <Card.Img variant="top" src={pic} alt="Card image" />
+        <Card.Body>
+        <Card.Title className='card-title'>RADEN MAS FARUQ </Card.Title>
+        <div className='visi'>
+          <table className="table-visi">
+              <tr>
+                  <th>
+                      <div className="header-th">Visi</div>
+                      <div className="content">1. Membangun negeri ramah</div>
+                  </th>
+              </tr>
+              <tr>
+                  <th>
+                      <div className="header-th">Misi</div>
+                      <div className="content">1. Membangun negeri ramah</div>
+                  </th>
+              </tr>
+           </table>
+          </div>
+        </Card.Body>
+      </Card>
+      <Card className='kiwkiw'>
+        <Card.Img variant="top" src={pic} alt="Card image" />
+        <Card.Body>
+        <Card.Title className='card-title'>RADEN MAS FARUQ </Card.Title>
+        <div className='visi'>
+          <table className="table-visi">
+              <tr>
+                  <th>
+                      <div className="header-th">Visi</div>
+                      <div className="content">1. Membangun negeri ramah</div>
+                  </th>
+              </tr>
+              <tr>
+                  <th>
+                      <div className="header-th">Misi</div>
+                      <div className="content">1. Membangun negeri ramah</div>
+                  </th>
+              </tr>
+           </table>
+          </div>
+        </Card.Body>
+      </Card>
+      </div>
+    </div>
+  )
 }
 
 function Index() {
@@ -32,3 +107,5 @@ function Index() {
     </>
   )
 }
+
+export default Index;
