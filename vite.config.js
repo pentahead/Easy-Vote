d1d3314311c346482940815c4a2dda5c8958f0a7
pentahead <dhiyaafaruq@gmail.com>
2024-12-07 09:15:46 +0700

    setup vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import sass from "sass";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ["date-fns/_lib/format/longFormatters"],
+  },
 });
